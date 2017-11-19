f3a865009b8e7c95f41fb427157dfbf63cc7a18e
himensuthar <himenmsuthar@gmail.com>
2017-11-19 12:06:04 +0530

    First Commit

--- SuperCoolApp/Startup.cs
+++ SuperCoolApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace SuperCoolApp
 {
@@ -15,20 +16,47 @@ namespace SuperCoolApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+
+            //Redirect any non API call to the Angular Application
+            //So our application can handle the routing
+            app.Use( async(context,next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                 !Path.HasExtension(context.Request.Path.Value) &&
+                 !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
             });
+
+
+            //Configure  application for usage  as API
+            //with default  route of /api/[controller]
+
+            app.UseMvcWithDefaultRoute();
+
+            //configure application to serv the index.html file from /wwwroot
+            // when you access the server from a web browser
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
